e1afff259cbe8c7fbf2f29bece5629eac5b5a790
Jonathan Bloedow <jhb4@cornell.edu>
2020-03-25 10:55:29 -0700

    Next step towards getting households via serialized population in environmental_sim working. Notes are in bash script.

---
+++ hinty_households/multiroute_with_hh/dtk_pre_process.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import regression_utils as ru
+import os
+
+def application( json_config_path ):
+    new_config_filename = "config.json"
+    ru.flattenConfig( json_config_path, new_config_filename  )
+    return new_config_filename 
