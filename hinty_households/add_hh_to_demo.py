3d63486d3ba10f352b86239b958642a58e957607
Jonathan Bloedow <jhb4@cornell.edu>
2020-03-28 10:02:08 -0700

    Enabled add_hh_to_demo to do Primary IPs as well as Household.

--- hinty_households/add_hh_to_demo.py
+++ hinty_households/add_hh_to_demo.py
@@ -10,38 +10,52 @@ if len( sys.argv ) < 3:
 demog_json = json.loads( open( sys.argv[1] ).read() )
 
 # TBD: Get the list of household ids from the previous script
+
+
+INTRA_HH_MULT = 10.0
+INTER_HH_MULT = 0.0
+
+def get_props( route, ip_key, num_ids ):
+    hh_prop = {}
+    hh_prop[ "Property" ] = ip_key
+    hh_prop[ "Values" ] = []
+    hh_prop[ "Initial_Distribution" ] = []
+    hh_prop[ "Transitions" ] = []
+    hh_prop[ "TransmissionMatrix" ] = {}
+    hh_prop[ "TransmissionMatrix" ][ route ] = {}
+    hh_prop[ "TransmissionMatrix" ][ route ][ "Matrix" ] = []
+
+    for hh_id in range( num_ids ):
+        hh_prop[ "Values" ].append( str( hh_id ) )
+        hh_prop[ "Initial_Distribution" ].append( 0 )
+    hh_prop[ "Initial_Distribution" ][0] = 1.0 # not this is nonsense but irrlevantly so for us.
+
+# OK, now create the matrix. num_idsxnum_ids with 1.0's on diagonal
+    for row in range(num_ids):
+        hh_prop[ "TransmissionMatrix" ][ route ][ "Matrix" ].append( list )
+        hh_prop[ "TransmissionMatrix" ][ route ][ "Matrix" ][row] = []
+        for col in range(num_ids):
+            if row == col:
+                hh_prop[ "TransmissionMatrix" ][ route ][ "Matrix" ][row].append( INTRA_HH_MULT )
+            else:
+                hh_prop[ "TransmissionMatrix" ][ route ][ "Matrix" ][row].append( INTER_HH_MULT  )
+    return hh_prop
+
+
+#demog_json[ "Defaults" ][ "IndividualProperties" ].append( hh_prop )
+primary_ids = json.loads( open( "primary_ids.json" ).read() )
+num_primaries = max(primary_ids)+3
+print( "No. Primaries = " + str( num_primaries ) )
+demog_json[ "Defaults" ][ "IndividualProperties" ][0] = get_props( "contact", "Primary", num_primaries )
+
 hh_ids = json.loads( open( "household_ids.json" ).read() )
 num_hhs = len(hh_ids) # should be 25, hunt down off-by-1 error
+print( "No. Households = " + str( num_hhs ) )
+if len(demog_json[ "Defaults" ][ "IndividualProperties" ] ) == 1:
+    demog_json[ "Defaults" ][ "IndividualProperties" ].append( get_props( "environmental", "Household", num_hhs ) )
+else:
+    demog_json[ "Defaults" ][ "IndividualProperties" ][1] = get_props( "environmental", "Household", num_hhs )
 
-INTRA_HH_MULT = 1.0
-INTER_HH_MULT = 0.1
-
-hh_prop = {}
-hh_prop[ "Property" ] = "Household"
-hh_prop[ "Values" ] = []
-hh_prop[ "Initial_Distribution" ] = []
-hh_prop[ "Transitions" ] = []
-hh_prop[ "TransmissionMatrix" ] = {}
-hh_prop[ "TransmissionMatrix" ][ "Route" ] = "Contact"
-hh_prop[ "TransmissionMatrix" ][ "Matrix" ] = []
-
-for hh_id in range( num_hhs ):
-    hh_prop[ "Values" ].append( str( hh_id ) )
-    hh_prop[ "Initial_Distribution" ].append( 0 )
-hh_prop[ "Initial_Distribution" ][0] = 1.0 # not this is nonsense but irrlevantly so for us.
-
-# OK, now create the matrix. num_hhsxnum_hhs with 1.0's on diagonal
-for row in range(num_hhs):
-    hh_prop[ "TransmissionMatrix" ][ "Matrix" ].append( list )
-    hh_prop[ "TransmissionMatrix" ][ "Matrix" ][row] = []
-    for col in range(num_hhs):
-        if row == col:
-            hh_prop[ "TransmissionMatrix" ][ "Matrix" ][row].append( INTRA_HH_MULT )
-        else:
-            hh_prop[ "TransmissionMatrix" ][ "Matrix" ][row].append( INTER_HH_MULT  )
-
-
-demog_json[ "Defaults" ][ "IndividualProperties" ].append( hh_prop )
 
 with open( sys.argv[2],"w+" ) as outfile:
     json.dump( demog_json, outfile, sort_keys=True, indent=4 )
